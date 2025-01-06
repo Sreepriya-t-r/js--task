6b2832eb2b2ea07f5983cd2374af590d324caa83
Sreepriya T R <sreepriyatr992@gmail.com>
2025-01-06 22:35:44 +0530

    first commit

---
+++ task2.js
@@ -0,0 +1,50 @@
+
+const prompt=require('prompt-sync')({sigint:true});
+// 1) Check if a word start with a specific letter
+// let str=prompt("word : ");
+// console.log(str.startsWith('K'));
+
+
+//  Replace spaces with Hyphens
+// Replace spaces with Hyphens
+// let str1 =prompt("word: ");
+// console.log(str1.replaceAll(" ","-"));
+
+
+
+//  1) Check if a word ends with a specific letter
+// let str=prompt("word : ");
+// console.log(str.endsWith('K'));
+
+// Split a paragraph into words of array.
+// let str3=prompt("paragraph : ");
+// console.log(str3.split(' '));
+
+
+//  check if a string includes a specific substring?
+// let str4=prompt("Substring or not: ");
+// console.log(str4.includes("world")); 
+
+
+// Write a program to capitalize the first and last character of each word in a string.
+
+
+
+//  using array methods and string methods reverse a string.
+// let str = prompt("Enter a string:");
+// console.log(str.split('').reverse().join(''));
+ 
+
+// //  Check whether a string is palindrome or not using array method.
+// let str = prompt("Enter a string:");
+// console.log(str===[str].reverse().join('') ? "Palindrome" : "Not a palindrome");
+
+
+// Given a 3-dimensional array, perform the following tasks:
+//   Convert the 3D array into a single-dimensional array.
+//   Find the highest number in the array.
+//   Remove the 3rd number (index 2) from the array and add 2 new numbers of your choice to the array.
+//  To check if a string is a palindrome
+//  1.  Construct a  3 dimentional array and covert it into a 2 dimentional array
+//  Str="7/8*2+12"  evaluate this string  without type conversion
+// 1.Remove an element at a specific index
