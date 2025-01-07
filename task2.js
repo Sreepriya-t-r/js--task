db7fa07eba384d079b280d619c8646d7516e819a
Sreepriya T R <sreepriyatr992@gmail.com>
2025-01-07 08:01:37 +0530

    second commit

--- task2.js
+++ task2.js
@@ -37,14 +37,42 @@ const prompt=require('prompt-sync')({sigint:true});
 
 // //  Check whether a string is palindrome or not using array method.
 // let str = prompt("Enter a string:");
-// console.log(str===[str].reverse().join('') ? "Palindrome" : "Not a palindrome");
+// let isPalindrome=str.toLowerCase().split('').reverse().join('');
+// console.log(isPalindrome);
 
 
 // Given a 3-dimensional array, perform the following tasks:
 //   Convert the 3D array into a single-dimensional array.
 //   Find the highest number in the array.
 //   Remove the 3rd number (index 2) from the array and add 2 new numbers of your choice to the array.
+
+// let array=[[17,[22,33],25],71,38];
+// let flat=array.flat(Infinity);
+// let newArray=flat.reduce((a,b)=>a>b?a:b);
+// flat.splice(2,1,34,100);
+
+// console.log(flat);
+
+
+
 //  To check if a string is a palindrome
-//  1.  Construct a  3 dimentional array and covert it into a 2 dimentional array
-//  Str="7/8*2+12"  evaluate this string  without type conversion
-// 1.Remove an element at a specific index
+// let str = prompt("Enter a string:");
+// let isPalindrome=str.toLowerCase().split('').reverse().join('');
+// console.log(isPalindrome);
+
+
+//  1.  Construct a  3 dimentional array and covert it into a 2 dimentional array.
+// let array = [[[1,2],[3,4]],[[5,6],[7,8]],[[9,10],[11,12]]];
+// let x=array.flat(1);
+// console.log(x);
+
+
+//  Str="7/8*2+12"  evaluate this string  without type conversion.
+let  Str="7/8*2+12";
+let x=eval(Str);
+console.log(x);
+
+// Remove an element at a specific index
+let array=[10,20,30,40,50];
+array.splice(2,1);
+console.log(array);
